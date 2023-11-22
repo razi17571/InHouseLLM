4f17bcc1a0f66c04de6e50cf066a276ccfa5ea42
razi17571 <ansari17571@gmail.com>
2023-11-22 16:33:32 +0530

    Fixed Question Answering Feature

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-# Load the BART model for summarization
 bart_model_name = r"C:\Projects\InhouseLLM\models\bart-large-cnn"
 bart_tokenizer = BartTokenizer.from_pretrained(bart_model_name)
 bart_model = BartForConditionalGeneration.from_pretrained(bart_model_name)
@@ -12,7 +11,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 bart_model.to(device)
 bart_model.eval()
 
-# Define the new models
 headline_model = T5ForConditionalGeneration.from_pretrained(r"C:\Projects\InhouseLLM\models\t5-base-en-generate-headline")
 headline_tokenizer = T5Tokenizer.from_pretrained(r"C:\Projects\InhouseLLM\models\t5-base-en-generate-headline")
 headline_model.to(device)
@@ -24,13 +22,10 @@ question_generation_model.to(device)
 question_generation_model.eval()
 
 question_answering_pipeline = pipeline("question-answering", model=r"C:\Projects\InhouseLLM\models\roberta-base-squad2")
-
 grammar_check_pipeline = pipeline("text-classification", model=r"C:\Projects\InhouseLLM\models\coedit-large")
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
 @app.route('/summarize', methods=['POST'])
 def summarize():
     if request.method == 'POST':
@@ -39,28 +34,15 @@ def summarize():
         summary_ids = bart_model.generate(input_ids, max_length=300, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True).to(device)
         summary = bart_tokenizer.decode(summary_ids[0], skip_special_tokens=True)
         return jsonify({'summary': summary})
-
-# @app.route('/generate_headline', methods=['POST'])
-# def generate_headline():
-#     if request.method == 'POST':
-#         text = request.form['text']
-#         input_ids = headline_tokenizer.encode(text, return_tensors='pt', max_length=1024, truncation=True).to(device)
-#         headline_ids = headline_model.generate(input_ids, max_length=150, num_beams=4, early_stopping=True).to(device)
-#         headline = headline_tokenizer.decode(headline_ids[0], skip_special_tokens=True)
-#         return jsonify({'headline': headline})
-
+    
 @app.route('/generate_headline', methods=['POST'])
 def generate_headline():
     if request.method == 'POST':
         text = request.form['text']
         input_ids = headline_tokenizer.encode(text, return_tensors='pt', max_length=1024, truncation=True)
-        
-        # Move the input data to the same device as the model
         input_ids = input_ids.to(device)
-
         headline_ids = headline_model.generate(input_ids, max_length=150, num_beams=4, early_stopping=True)
-        headline_ids = headline_ids.to(device)  # Ensure that generated output is on the same device
-
+        headline_ids = headline_ids.to(device)  
         headline = headline_tokenizer.decode(headline_ids[0], skip_special_tokens=True)
         return jsonify({'headline': headline})
 
@@ -71,22 +53,18 @@ def generate_questions():
         input_ids = question_generation_tokenizer.encode(text, return_tensors='pt', max_length=1024, truncation=True).to(device)
         questions_ids = question_generation_model.generate(input_ids, max_length=150, num_beams=4, early_stopping=True).to(device)
         questions = question_generation_tokenizer.decode(questions_ids[0], skip_special_tokens=True).split('<sep>')
-        return jsonify({'questions': questions[:-2]})
+        return jsonify({'questions': questions[:-1]})
 
-@app.route('/question_answering', methods=['POST'])
-def question_answering():
+@app.route('/question_answering_batch', methods=['POST'])
+def question_answering_batch():
     if request.method == 'POST':
-        question = request.form['question']
+        questions = request.form['questions'].split('\n')
         context = request.form['context']
-        answer = question_answering_pipeline(question=question, context=context)
-        return jsonify({'answer': answer})
-
-@app.route('/grammar_check', methods=['POST'])
-def grammar_check():
-    if request.method == 'POST':
-        text = request.form['text']
-        result = grammar_check_pipeline(text)
-        return jsonify({'result': result})
+        answers = []
+        for question in questions:
+            answer = question_answering_pipeline(question=question, context=context)
+            answers.append({'question': question, 'answer': answer['answer']})
+        return jsonify({'questionsAndAnswers': answers})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -76,57 +76,113 @@ function generateQuestions() {
     });
 }
 
-function questionAnswering() {
-    var userInput = document.getElementById('userInput').value;
-    var question = prompt("Please enter a question related to the input text:");
-    if (question) {
-        var resultArea = document.getElementById('resultArea');
-        var loading = document.getElementById('loading');
+// Modify the questionAnswering function
+function prepareForQuestionAnswering() {
+    // Show the textarea for entering questions
+    document.getElementById('userQuestions').style.display = 'block';
 
-        resultArea.innerHTML = '';
-        loading.style.display = 'block';
+    // Disable the button to avoid multiple clicks
+    document.getElementById('questionAnsweringButton').disabled = true;
 
-        fetch('/question_answering', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-            },
-            body: new URLSearchParams({ 'question': question, 'context': userInput }).toString()
-        })
-        .then(response => response.json())
-        .then(data => {
-            loading.style.display = 'none';
-            resultArea.innerHTML = '<p><strong>Answer:</strong></p><p>' + data.answer + '</p>';
-        })
-        .catch(error => {
-            loading.style.display = 'none';
-            resultArea.innerHTML = '<p>An error occurred while answering the question.</p>';
-        });
-    }
+    // Change the onclick event to call the actual questionAnswering function
+    document.getElementById('questionAnsweringButton').onclick = function () {
+        questionAnswering();
+    };
 }
 
-function grammarCheck() {
+function toggleQuestionAnswering() {
+    var questionAnsweringSection = document.getElementById('questionAnsweringSection');
+    questionAnsweringSection.style.display = (questionAnsweringSection.style.display === 'none') ? 'block' : 'none';
+}
+
+function submitQuestions() {
+    toggleQuestionAnswering()
     var userInput = document.getElementById('userInput').value;
+    var questionInput = document.getElementById('questionInput').value;
     var resultArea = document.getElementById('resultArea');
     var loading = document.getElementById('loading');
 
     resultArea.innerHTML = '';
     loading.style.display = 'block';
 
-    fetch('/grammar_check', {
+    fetch('/question_answering_batch', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         },
-        body: new URLSearchParams({ 'text': userInput }).toString()
+        body: new URLSearchParams({ 'questions': questionInput, 'context': userInput }).toString()
     })
     .then(response => response.json())
     .then(data => {
         loading.style.display = 'none';
-        resultArea.innerHTML = '<p><strong>Grammar Check Result:</strong></p><p>' + data.result + '</p>';
+        resultArea.innerHTML = '<p><strong>Questions and Answers:</strong></p>' + formatQA(data.questionsAndAnswers);
     })
     .catch(error => {
         loading.style.display = 'none';
-        resultArea.innerHTML = '<p>An error occurred while checking grammar.</p>';
+        resultArea.innerHTML = '<p>An error occurred while answering the questions.</p>';
     });
+}
+
+function formatQA(questionsAndAnswers) {
+    return questionsAndAnswers.map(qa => '<p><strong>Question:</strong> ' + qa.question + '</p><p><strong>Answer:</strong> ' + qa.answer + '</p>').join('');
+}
+
+// Function to format questions and answers for display
+function formatQuestionsAndAnswers(questionsAndAnswers) {
+    var formattedHTML = '<ul>';
+    questionsAndAnswers.forEach(qa => {
+        formattedHTML += '<li><strong>Question:</strong> ' + qa.question + '<br><strong>Answer:</strong> ' + qa.answer + '</li>';
+    });
+    formattedHTML += '</ul>';
+    return formattedHTML;
+}
+
+
+// function grammarCheck() {
+//     var userInput = document.getElementById('userInput').value;
+//     var resultArea = document.getElementById('resultArea');
+//     var loading = document.getElementById('loading');
+
+//     resultArea.innerHTML = '';
+//     loading.style.display = 'block';
+
+//     fetch('/grammar_check', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+//         },
+//         body: new URLSearchParams({ 'text': userInput }).toString()
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         loading.style.display = 'none';
+//         resultArea.innerHTML = '<p><strong>Grammar Check Result:</strong></p><p>' + data.result + '</p>';
+//     })
+//     .catch(error => {
+//         loading.style.display = 'none';
+//         resultArea.innerHTML = '<p>An error occurred while checking grammar.</p>';
+//     });
+// }
+
+// Modify the handleFileUpload function in your script.js
+function handleFileUpload() {
+    var fileInput = document.getElementById('fileInput');
+    var userInput = document.getElementById('userInput');
+
+    var file = fileInput.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        userInput.value = e.target.result;
+
+        // Reset the value of the file input to allow selecting the same file again
+        fileInput.value = '';
+    };
+
+    if (file) {
+        reader.readAsText(file);
+    } else {
+        // If no file is selected, clear the textarea
+        userInput.value = '';
+    }
 }
